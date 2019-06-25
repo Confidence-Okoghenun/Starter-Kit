7565d78ed53f83619c42d30a7adc1ec9dfa19979
Confidence Okoghenun <confidenceboi@gmail.com>
2019-06-25 21:56:13 +0100

    feat: Added transpiling with Babel

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Starter-Kit
+
+Project contains
+
+1. Editor Config
+2. Npm scripts for automation
+3. Transpiling with babel
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
-const chalk = require('chalk');
+import chalk from 'chalk';
 console.log(chalk.yellow('##############################'));
 console.log(chalk.yellow('##') ,chalk.green('Starting app in dev mode'), chalk.blue('##'));
 console.log(chalk.blue('##############################'));
